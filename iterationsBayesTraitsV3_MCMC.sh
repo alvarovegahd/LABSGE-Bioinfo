92f9927d35dd6f76ee3f667c92ca16cfef1ce847
alvarovegahd <alvarovegahd@gmail.com>
2017-03-29 17:13:57 -0600

    Important changes to fix the compatibility with Tracer. Code comments and interface with user were improved.

--- iterationsBayesTraitsV3_MCMC.sh
+++ iterationsBayesTraitsV3_MCMC.sh
@@ -4,8 +4,11 @@
 # 2017
 # LABSGE
 
-# This variables should be specified by the user,
-# and are the files needed by BayesTraitsV3
+echo "This program should be executed in the same folder as a trees file, commands file, traits file and BayesTraitsV3 file"
+echo "Make sure you have changed the parameters traits_File, trees_File, commands_File, number_of_iterations with a text editor on the current program code"
+
+read -p "Please press ENTER" var
+
 traits_File="MammalBrainBody.txt"
 trees_File="Mammal.trees"
 commands_File="MammalCommands.txt"
@@ -30,7 +33,7 @@ for i in $(seq 1 $number_of_iterations); do
 	#Quit head
 	initial_line=`grep -wn Iteration ./raw_Results/$traits_File$i".Log.txt" |cut -f1 -d:`
         final_line=`wc -l ./raw_Results/$traits_File$i".Log.txt" |cut -f1 -d' '`
-	output_length=$(($final_line-$initial_line))
+	output_length=$((1+$final_line-$initial_line))
         #Save Tracer files
         tail -n $output_length ./raw_Results/$traits_File$i".Log.txt" > ./logs_to_Tracer/$traits_File$i"toTracer.Log.txt"
 
