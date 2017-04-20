ee11ae8b5c575a314c4053c5f9d1117910cb8591
alvarovegahd <alvarovegahd@gmail.com>
2017-04-19 19:09:07 -0600

    v2 of phylonet

--- phylonet.R
+++ phylonet.R
@@ -1,32 +1,46 @@
+# The user should change the last two lines of this file and then, run the entire code
 
-
-# Load required libraries and set working directory and species file
+#--------------------------------------------------------------------------------------------------------------------
+# Load required libraries and set working directory
 library("rentrez")
-workingDirectory="/home/alvaro/Documents/Phylonet"
-setwd(workingDirectory)
-file<-"species.csv"
-# Load species file
-species<-read.csv("species.csv")
-
-# Set vector of search keywords
-gene_Search= ("18S 18s")
 
 # This function downloads the specified number of sequences per species of a specified gene
 download_sequences = function(speciesFile,gene_Search,nSeq=1,
-                              format="fasta",path=getwd(),db="nucleotide"){
-  
-# In each iteration, a multifasta file is downloaded per species
+                              format="fasta",path=getwd(),db="nucleotide",multi=T){
+  # To generate logfile
+  logFile <<- file("phylonetRun.log")
+  sink(logFile, append=TRUE)
+  sink(logFile, append=TRUE, type="message")
+
+  # In each iteration, a multifasta file is downloaded per species
   line=1
-  
+
   for(line in 1:length(species$Sp)){
     search<-entrez_search(db,
                           term=paste(species$Sp[line],"[ORGN]","&", gene_Search),
-                          use_history=TRUE)
-    all_recs <- entrez_fetch(db="nucleotide", web_history = search$web_history, rettype=format)
-    write(all_recs,file=paste(species$Sp[line],".",format,sep = ""))
+                          use_history=TRUE,retmax=nSeq)
     print(paste("Number of sequences of the species ",species$Sp[line],":",sep = ""))
     print(search$count)
-    }
-}
+    
+    if(search$count == 0){ # Avoid creating empty files
+      print(paste("WARNING: There are no sequences available given the search terms for the gene ",gene_Search, " of the species ",species$Sp[line],sep=""))
+    } 
 
-download_sequences(species,gene_Search)
+    else {
+
+    all_recs <- entrez_fetch(db="nucleotide", web_history = search$web_history, rettype=format,retmax=nSeq)
+
+    if(multi){
+      write(all_recs,file=paste("PhylonetRun",Sys.Date(),".",format,sep = ""),append=T)
+    } 
+    else {
+    cat(all_recs,file=paste(species$Sp[line],".",format,sep = ""))
+    }
+    }
+  }
+  closeAllConnections() # Close console connection to logfile
+}
+#----------------------------------------------------------------------------------------------------------------------------
+# The user just have to change the following two lines
+setwd("/home/alvaro/Documents/Phylonet") # Set working directory
+download_sequences(speciesFile="species.csv",gene_Search="18s 18S",nSeq=3)
